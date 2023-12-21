b554dfa6f1f504f8404fefd66c5f4871e3836659
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-21 20:50:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let count = 0;
+const counterObj = document.getElementById("counter");
+const btnObj = document.getElementById("incrementBtn");
+
+btnObj.addEventListener("click", () => {
+    alert("Un-incremented value: " + count);
+    count++;
+    counterObj.innerText = count;
+});
